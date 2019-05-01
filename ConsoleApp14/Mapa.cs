873d05972bb73d302d3a5775b19f29d6e01aba70
itmayer <itmayer@miuandes.cl>
2019-05-01 15:48:36 -0400

    mapa

--- ConsoleApp14/Mapa.cs
+++ ConsoleApp14/Mapa.cs
@@ -4,8 +4,30 @@ namespace ConsoleApp14
 {
     public class Mapa
     {
-        Bitmon[,,] Bitmons;
-        Terreno[,] Terrenos;
+        Bitmon[,,] Bitmons; // aqui van los bitmons con sus posiciones iniciales
+        Terreno[,] Terrenos; // aqui va el mapa inicial con sus terrenos
+
+        string ImprimirMapa()
+        {
+
+            for (int fila = 0; fila < 6; fila++)
+            {
+                for (int col = 0; col < 6; col++)
+                {
+                    Console.Write(" " + opcion3[fila, col].GetTerreno());
+                }
+                
+            }
+        }
+       
+         for (int fila = 0; fila< 6; fila++)
+            {
+                for (int col = 0; col< 6; col++)
+                {
+                    Console.Write(" " + opcion3[fila, col].GetTerreno());
+                }
+                Console.WriteLine(" ");
+            }
 
     }
 }
